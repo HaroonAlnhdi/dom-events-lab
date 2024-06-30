ec5d27219970705eee20a45f61d38ebda946f1d0
Haroon <you@example.com>
2024-06-30 09:06:46 +0300

    puch

--- app.js
+++ app.js
@@ -1,131 +1,108 @@
-/*-------------------------------- Constants --------------------------------*/
-    const number = document.querySelectorAll('.number');
-    //const operator = document.querySelectorAll('.operator');
-   
-    const display = document.querySelector('.display');
-/*-------------------------------- Variables --------------------------------*/
-    let numberdisplay;
-    let displayperator;
-    let firstNumber = null;
-    let secondNumber = null;
-    let operator = null;
-    let result = 0;
-    
-/*------------------------ Cached Element References ------------------------*/
+ 
+        /*-------------------------------- Constants --------------------------------*/
+        const numberButtons = document.querySelectorAll('.number');
+        const operatorButtons = document.querySelectorAll('.operator');
+        const display = document.querySelector('.display');
+        const equalButton = document.querySelector('.equals');
 
-/*----------------------------- Event Listeners -----------------------------*/
+        /*-------------------------------- Variables --------------------------------*/
+        let firstNumber = '';
+        let secondNumber = '';
+        let operator = null;
+        let result = 0;
+
+        /*----------------------------- Event Listeners -----------------------------*/
+        numberButtons.forEach(button => {
+            button.addEventListener('click', function(event) {
+                handleNumber(event.target.innerText);
+            });
+        });
+
+        operatorButtons.forEach(button => {
+            button.addEventListener('click', function(event) {
+                handleOperator(event.target.innerText);
+            });
+        });
 
 
+       // clearButton.addEventListener('click', clearDisplay);
+        equalButton.addEventListener('click', calculateResult);
 
-document.addEventListener('click', function(event) {
-    
-    console.log(event.target.innerText);
-
+        calculator.addEventListener('click', (event) => {
+            
+            console.log(event.target.innerText);
+          
         
-    if (event.target.classList.contains('number')) {
-        if (operator === null) {
-            // Set the first number if no operator is selected yet
-            firstNumber = event.target.innerText;
-            display.innerHTML += firstNumber;
-        } else {
-            // Set the second number if the operator is already selected
-            secondNumber = event.target.innerText;
-            display.innerHTML += secondNumber;
+            // Example
+            if (event.target.innerText === 'C') {
+             clearDisplay();
+            }
+          });
+
+        /*-------------------------------- Functions --------------------------------*/
+        function handleNumber(number) {
+            if (operator === null) {
+                firstNumber += number;
+                display.innerHTML = firstNumber;
+            } else {
+                secondNumber += number;
+                display.innerHTML = firstNumber + operator + secondNumber;
+            }
         }
-    }
 
-    if (event.target.classList.contains('operator')) {
-        operator = event.target.innerText;
-        display.innerHTML += operator;
-    }
+        function handleOperator(op) {
+            if (firstNumber !== '') {
+                operator = op;
+                display.innerHTML = firstNumber + operator;
+            }
+        }
 
-        if (firstNumber !== null && secondNumber !== null && operator === '*') {
-            result = multiplications(firstNumber, secondNumber);
-            
-            // Reset the state for next calculation
-            firstNumber = null;
-            secondNumber = null;
+        function clearDisplay() {
+            display.innerHTML = '';
+            firstNumber = '';
+            secondNumber = '';
             operator = null;
-            
+            result = 0;
         }
 
-        else if (firstNumber !== null && secondNumber !== null && operator === '/') {
-            result = divided(firstNumber, secondNumber);
-           
-           // Reset the state for next calculation
-           firstNumber = null;
-           secondNumber = null;
-           operator = null;
-           
-       }
-
-       else if  (firstNumber !== null && secondNumber !== null && operator === '-') {
-        result = sub(firstNumber, secondNumber);
-       
-       // Reset the state for next calculation
-       firstNumber = null;
-       secondNumber = null;
-       operator = null;
-       
-      }
-
-
-      else if (firstNumber !== null && secondNumber !== null && operator === '+') {
-        result = sum(firstNumber, secondNumber);
-       
-       // Reset the state for next calculation
-       firstNumber = null;
-       secondNumber = null;
-       operator = null;
-       
-   }
-    
-
-    if (event.target.innerText === 'C') {
-        display.innerHTML = " "
-        firstNumber = null;
-        secondNumber = null;
-        operator = null;
-      }
-
-      if ( event.target.innerText === '=') {
-      
-        display.innerHTML = result;
-        // Reset the state for next calculation
-        firstNumber = null;
-        secondNumber = null;
-        operator = null;
-    }
-
-
-    });
-
-
-/*-------------------------------- Functions --------------------------------*/
-
-
-
-                function multiplications(num1, num2) {
-                    return num1 * num2;
+        function calculateResult() {
+            if (firstNumber !== '' && secondNumber !== '' && operator !== null) {
+                let num1 = parseFloat(firstNumber);
+                let num2 = parseFloat(secondNumber);
+                
+                if (operator === '+') {
+                    result = sum(num1, num2);
+                } else if (operator === '-') {
+                    result = sub(num1, num2);
+                } else if (operator === '*') {
+                    result = multiplications(num1, num2);
+                } else if (operator === '/') {
+                    result = divided(num1, num2);
                 }
+                
+                display.innerHTML = result;
+                firstNumber = result.toString();
+                secondNumber = '';
+                operator = null;
+            }
+        }
 
+        function multiplications(num1, num2) {
+            return num1 * num2;
+        }
 
-                function divided(num1 ,num2){
-                    
-                    if(num2 === '0'){
+        function divided(num1, num2) {
+            if (num2 === 0) {
+                return "Error";
+            } else {
+                return num1 / num2;
+            }
+        }
 
-                   result = " Error ";
-                   return result;
-                    }
-                    else
-                    return num1/num2;
-                }
+        function sub(num1, num2) {
+            return num1 - num2;
+        }
 
-                function sub (num1,num2){
-
-                    return num1 - num2 ;
-                }
-
-                function sum(num1,num2){
-                    return num1 + num2 ;
-                }
+        function sum(num1, num2) {
+            return num1 + num2;
+        }
